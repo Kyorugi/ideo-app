c0f1e08caafcdd33252c4deb4fa59fbd667569a8
Kamil Chwesiuk <kamil.chwesiuk@gmail.com>
2023-06-06 14:38:47 +0200

    vite, prettier & eslint config

---
+++ vite-project-ideo-app/.eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "some-other-config-you-use",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": "warn",
+    curly: ["error", "all"],
+    "lines-around-comment": [
+      "error",
+      {
+        beforeBlockComment: true,
+        afterBlockComment: true,
+        beforeLineComment: true,
+        afterLineComment: true,
+        allowBlockStart: true,
+        allowBlockEnd: true,
+        allowObjectStart: true,
+        allowObjectEnd: true,
+        allowArrayStart: true,
+        allowArrayEnd: true,
+      },
+    ],
+  },
+};
