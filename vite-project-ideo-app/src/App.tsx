41ced9ad80cf2ce77ccb87572edb1017686125db
Kamil Chwesiuk <kamil.chwesiuk@gmail.com>
2023-06-10 13:52:28 +0200

    getting data from dataJson and assigning types

--- vite-project-ideo-app/src/App.tsx
+++ vite-project-ideo-app/src/App.tsx
@@ -1,8 +1,45 @@
 import teleLogo from "./assets/telelogo.jpg";
-
 import "./App.css";
+import jsonData from "./api/data.json";
+import { useState, useEffect } from "react";
+
+interface Service {
+  name: string;
+  prices: {
+    [year: string]: number;
+  };
+  dependantServices?: string;
+}
+
+interface ServicePackage {
+  name: string;
+  prices: {
+    [year: string]: number;
+  };
+  services: string[];
+}
+interface Data {
+  services: Service[];
+  servicePackages: ServicePackage[];
+}
 
 function App() {
+  const [data, setData] = useState<Data | null>(null);
+
+  const fetchData = async () => {
+    try {
+      setData(jsonData);
+    } catch (error) {
+      console.log("Wystąpił błąd podczas pobierania danych", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  console.log(data);
+
   return (
     <>
       <header>
